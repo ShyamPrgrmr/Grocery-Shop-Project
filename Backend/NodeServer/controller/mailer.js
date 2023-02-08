4f611e9f9b1683d2aa44f5bd97917b0f53c5779b
Shyam G. Pradhan <53821445+ShyamPrgrmr@users.noreply.github.com>
2023-02-08 23:29:05 +0530

    Update mailer.js
    Added AWS mailing

--- Backend/NodeServer/controller/mailer.js
+++ Backend/NodeServer/controller/mailer.js
@@ -1,4 +1,67 @@
-const nodemailer = require('nodemailer');
+var AWS = require('aws-sdk');
+
+exports.Mailer =async(receiver,message) =>{
+
+AWS.config.update({region: process.env.AWS_REGION}); //create environmet variable for REGION
+
+let mailOptions = {
+    from: process.env.SENDER_EMAIL, //create environment variable
+    to: receiver.email, 
+    subject: message.subject, 
+    text: message.text, 
+    html: message.html
+};
+
+var params = {
+  Destination: { 
+    CcAddresses: [
+      mailOptions.to,
+    ],
+    ToAddresses: [
+      mailOptions.to,
+    ]
+  },
+  Message: { 
+    Body: { 
+      Html: {
+       Charset: "UTF-8",
+       Data: mailOptions.html
+      },
+      Text: {
+       Charset: "UTF-8",
+       Data: mailOptions.text
+      }
+     },
+     Subject: {
+      Charset: 'UTF-8',
+      Data: mailOptions.subject
+     }
+    },
+  Source: mailOptions.from, 
+  ReplyToAddresses: []
+};
+
+var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+
+
+sendPromise.then(
+  function(data) {
+    console.log("Email has been send");
+    return data; 
+  }).catch(
+    function(err) {
+        console.log(err);
+        return(err);
+  });
+
+
+}
+
+
+
+
+
+/*const nodemailer = require('nodemailer');
 
 exports.Mailer=async(receiver,message)=>{
 
@@ -16,15 +79,9 @@ exports.Mailer=async(receiver,message)=>{
     });
 
 
-    let mailOptions = {
-        from: 'grocery.shop50@hotmail.com', 
-        to: receiver.email, 
-        subject: message.subject, 
-        text: message.text, 
-        html: message.html
-    };
+   
 
 
     let data = await transporter.sendMail(mailOptions);
     return data;
-}
+}*/
